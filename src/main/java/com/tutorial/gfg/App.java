99de79fdf1d217b195dcbdf3903c9a31959e3af8
kishore routhu <kishore.routhu5@gmail.com>
2017-01-29 20:48:29 +0530

    Initial Commit

---
+++ src/main/java/com/tutorial/gfg/App.java
@@ -0,0 +1,13 @@
+package com.tutorial.gfg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
