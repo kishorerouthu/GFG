2a1aae83412b99b5103af83cb8e65116e6050a8a
kishore routhu <kishore.routhu5@gmail.com>
2017-06-15 18:34:09 +0530

    Added reverse array problem

--- src/main/java/com/tutorial/gfg/arrays/MergeArrays.java
+++ src/main/java/com/tutorial/gfg/arrays/MergeArrays.java
@@ -2,7 +2,16 @@ package com.tutorial.gfg.arrays;
 
 /**
  * Created by Kishore Routhu on 15/6/17 6:04 PM.
+ *
+ * Merge an array of size n into another array of size m+n
+ *
+ * Question:
+ * There are two sorted arrays. First one is of size m+n containing only m elements.
+ * Another one is of size n and contains n elements.
+ * Merge these two arrays into the first array of size m+n such that the output is sorted.
+ *
  */
+
 public class MergeArrays {
 
     //Consider NA as -1 in arrays
