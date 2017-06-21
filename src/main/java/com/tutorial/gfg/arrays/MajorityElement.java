8b084a7f69a3f654a4073a1adf44af532873f778
kishore routhu <kishore.routhu5@gmail.com>
2017-06-21 21:24:49 +0530

    Added Majority Element problem

--- src/main/java/com/tutorial/gfg/arrays/MajorityElement.java
+++ src/main/java/com/tutorial/gfg/arrays/MajorityElement.java
@@ -24,7 +24,13 @@ public class MajorityElement {
         //int a[] = new int[]{3, 3, 4, 2, 4, 4, 2, 4, 4};
         int a[] = new int[]{3, 3, 4, 2, 4, 4, 2, 4};
         int n = 8;
-        printMajorityElement(a, n);
+       // printMajorityElement(a, n);
+
+        //Is Majority Element
+        int arr[] = {1, 2, 3, 3, 3, 3, 10};
+        int n1 = arr.length;
+        int x = 3;
+        System.out.println(isMajorityElementBSearch(arr, n1, x));
     }
 
     /*
@@ -43,5 +49,47 @@ public class MajorityElement {
                 map.put(a[i], count);
         }
         System.out.println("NONE");
+    }
+
+    /*
+        Method 1: Is majority element using linear search
+     */
+    private static boolean isMajorityElement(int a[], int n, int x) {
+        for (int i = 0; i < n; i++)
+            if (a[i] == x)
+                return a[i + n/2] == x;
+
+        return false;
+    }
+
+    /*
+        Method 2 : Using binary search
+
+     */
+    private static boolean isMajorityElementBSearch(int a[], int n, int x) {
+
+        int i = binarySearch(a, 0, n-1, x);
+
+        if (i == -1)
+            return false;
+
+        return ((i + n/2 <= (n-1)) && (a[i + n/2] == x));
+
+    }
+
+    private static int binarySearch(int a[], int l, int r, int x) {
+
+        if (l <= r) {
+            int m = (l+r) >> 1;
+
+            if (m == 0 || (x > a[m-1] && a[m] == x))
+                return m;
+            else if (x < a[m])
+                return binarySearch(a, m+1, r, x);
+
+            return binarySearch(a, l, m-1, x);
+        }
+
+        return -1;
     }
 }
