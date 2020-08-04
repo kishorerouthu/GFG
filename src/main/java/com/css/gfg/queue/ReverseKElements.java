749e322f9bd621066fbfde7022fcdb16d78f2aa0
KishoreRouthu <kishore.routhu5@gmail.com>
2020-08-04 09:25:07 +0530

    Added: Reverse K elements in the queue

---
+++ src/main/java/com/css/gfg/queue/ReverseKElements.java
@@ -0,0 +1,61 @@
+package com.css.gfg.queue;
+
+import java.util.Stack;
+
+/**
+ * Given an integer k and a queue of integers, we need to reverse the order of the first k elements of the queue, leaving the other elements in the same relative order.
+ *
+ * Only following standard operations are allowed on queue.
+ *
+ * enqueue(x) : Add an item x to rear of queue
+ * dequeue() : Remove an item from front of queue
+ * size() : Returns number of elements in queue.
+ * front() : Finds front item.
+ * Examples:
+ *
+ * Input : Q = [10, 20, 30, 40, 50, 60,
+ *             70, 80, 90, 100]
+ *         k = 5
+ * Output : Q = [50, 40, 30, 20, 10, 60,
+ *              70, 80, 90, 100]
+ *
+ * Input : Q = [10, 20, 30, 40, 50, 60,
+ *             70, 80, 90, 100]
+ *         k = 4
+ * Output : Q = [40, 30, 20, 10, 50, 60,
+ *              70, 80, 90, 100]
+ */
+public class ReverseKElements {
+
+    /**
+     * Method1 : Reverse using Queue & Dequeue
+     * 1. Push first K elements to Queue
+     * 2. Enqueue all the K elements from Queue to Dequeue
+     */
+    public static void reverse() {
+        int a[] = new int[] {50, 40, 30, 20, 10, 60, 70, 80, 90, 100};
+        int k = 5;
+        Queue<Integer> q = new ArrayLinearQueue(k);
+        Dequeue dequeue = new Dequeue(a.length);
+        for(int data : a) {
+            dequeue.enqueueRear(data);
+        }
+        int c=0;
+        while (c < k) {
+            q.enqueue(dequeue.dequeueFront());
+            c++;
+        }
+
+        while (!q.isEmpty()) {
+            dequeue.enqueueFront(q.dequeue());
+        }
+
+        dequeue.display();
+    }
+
+
+    public static void main(String[] args) {
+        reverse();
+    }
+
+}
